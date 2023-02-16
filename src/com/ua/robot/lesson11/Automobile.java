1cf3ccfee81a6f1984096c9f882ab5fa7146accd
Oleh Nipruk <nipruk@ukr.net>
2023-02-16 18:57:26 +0200

    lesson11 commit

---
+++ src/com/ua/robot/lesson11/Automobile.java
@@ -0,0 +1,20 @@
+package com.ua.robot.lesson11;
+
+public class Automobile {
+    private int year;
+    private int price;
+
+    public Automobile() {
+    }
+
+    public Automobile(int year, int price) {
+        this.year = year;
+        this.price = price;
+    }
+
+    public void returnType() {
+        System.out.println("Automobile");
+    }
+}
+
+
---
+++ src/com/ua/robot/lesson11/Main.java
@@ -0,0 +1,12 @@
+package com.ua.robot.lesson11;
+
+public class Main {
+    public static void main(String[] args) {
+        Automobile automobile = new Automobile();
+        automobile.returnType();
+        Passengercar passengercar = new Passengercar();
+        passengercar.returnType();
+        Truck truck = new Truck();
+        truck.returnType();
+    }
+}
