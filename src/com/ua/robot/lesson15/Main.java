0e09d2d63c3dc6450db527da66e38548cb9727bb
Oleh Nipruk <nipruk@ukr.net>
2023-03-01 20:49:23 +0200

    lesson15 commit

---
+++ src/com/ua/robot/lesson15/Main.java
@@ -0,0 +1,11 @@
+package com.ua.robot.lesson15;
+
+public class Main {
+    public static void main(String[] args) {
+        Driver biker = new Driver();
+        biker.drive();
+        Pilot bird = new Pilot();
+        bird.fly();
+    }
+}
+
